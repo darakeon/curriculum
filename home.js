1e98b3266b85477ef1d2a9c374a669cf8d2c4ce4
Dara Keon <lc.sales.duarte@gmail.com>
2021-06-11 20:35:15 -0300

    dates: order dated lists descending

--- home.js
+++ home.js
@@ -4,6 +4,11 @@ const fs = require('fs')
 const hostname = '127.0.0.1'
 const port = 3000
 
+const current = {
+	'EN': 'current',
+	'PT': 'atual',
+}
+
 const server = http.createServer((req, res) => {
 	if (req.url == '/favicon.ico')
 		return exit(res, 404, 'text', null)
@@ -55,21 +60,26 @@ function process(html, data, lang, parent) {
 		const content = data[key]
 		const regex = new RegExp(`{{${key}}}`, 'g')
 
-		if (typeof(content) != 'string' && content.length) {
-			const childHtml = getHtml(processing)
+		if (!content) continue;
 
+		if (typeof(content) != 'string' && content.length) {
+
+			const processed = processStartAndEnd(content, lang)
+
+			const childHtml = getHtml(processing)
 			const children = []
 
-			for(const i in content) {
+			for(const i in processed) {
 				children.push(
 					process(
 						childHtml, 
-						content[i], 
+						processed[i], 
 						lang, 
 						processing
 					)
 				)
 			}
+
 			html = html.replace(regex, children.join('\n'))
 
 		} else {
@@ -86,6 +96,52 @@ function process(html, data, lang, parent) {
 	}
 
 	return html.replace(/{{.+}}/g, '');
+}
+
+function processStartAndEnd(content, lang) {
+	if (!content[0].Start) return content
+
+	content = content.sort((a, b) => {
+		if (a.End == b.End) {
+			if (a.Start < b.Start)
+				return +1
+			if (a.Start > b.Start)
+				return -1
+			return 0;
+		}
+
+		if (!b.End || a.End < b.End)
+			return +1
+		if (!a.End || a.End > b.End)
+			return -1
+	})
+
+	content = content.map((e) => {
+		e.StartText = toDate(e.Start, lang)
+
+		e.EndText = e.End
+			? toDate(e.End, lang)
+			: current[lang].toUpperCase();
+
+		return e
+	})
+
+	return content
+}
+
+function toDate(text, lang) {
+	const parts = text.split('-')
+	switch (parts.length) {
+		case 2:
+			const month = new Date(0, parts[1])
+				.toLocaleString(lang, { month: 'short' })
+				.replace(".", "")
+				.toUpperCase();
+
+			return `${month}/${parts[0]}`
+		default:
+			return text;
+	}
 }
 
 function getHtml(name) {
