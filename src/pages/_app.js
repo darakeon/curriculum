cd5329cf2445ea3ce9ee4014fed1c6ad7b4ac229
Dara Keon <lc.sales.duarte@gmail.com>
2024-12-09 23:41:11 +0000

    react: fix quotes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
-import '../styles/global.css';
-import '../styles/lucas-carol.css';
+import "../styles/global.css"
+import "../styles/lucas-carol.css"
 
 export default function App({ Component, pageProps }) {
 	return <Component {...pageProps} />
