f93f7ceb43abe021adc3e5a2fb32e210b9615fe9
Dara Keon <lc.sales.duarte@gmail.com>
2025-12-25 17:30:13 +0000

    site: fix dates order

--- src/utils/sort-by-dates.ts
+++ src/utils/sort-by-dates.ts
@@ -3,11 +3,8 @@ import Dateable from "../domain/dateable"
 
 function sortByDates<TDateable extends Dateable>(collection: TDateable[]): TDateable[] {
 	return collection.sort((a, b): number => {
-		if (a.end == b.end) {
-			if (a.start < b.start) return +1
-			if (a.start > b.start) return -1
-			return 0
-		}
+		if (a.start < b.start) return +1
+		if (a.start > b.start) return -1
 
 		if (!b.end || (a.end && a.end < b.end)) return +1
 		if (!a.end || (b.end && a.end > b.end)) return -1
